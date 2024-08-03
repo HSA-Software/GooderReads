139d0552cc2981cb867521c017a551b80b0da7f2
Adam Hewitt <ahewitt@glasswall.com>
2024-08-03 16:48:10 +0100

    add authorId to Book object for linking existing authors to books

--- Source/GooderReads/GooderReads.ApiService/Mutations/Books.cs
+++ Source/GooderReads/GooderReads.ApiService/Mutations/Books.cs
@@ -5,6 +5,12 @@ public class BooksMutation
     public async Task<Book> AddBook(Book book, GooderReadsContext gooderReadsContext)
     {
         book.Id = null;
+
+        if (book.AuthorId.HasValue)
+        {
+            book.Author = null;
+        }
+
         var result = await gooderReadsContext.Books.AddAsync(book);
         await gooderReadsContext.SaveChangesAsync();
         return result.Entity;
