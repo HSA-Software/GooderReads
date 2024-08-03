f4f222c8770c70dd156469873f7dc4380b635ca0
Adam Hewitt <ahewitt@glasswall.com>
2024-08-03 14:54:43 +0100

    configure faker for book and author

--- Source/GooderReads/GooderReads.ApiService/FakeDatabase/FakeBooks.cs
+++ Source/GooderReads/GooderReads.ApiService/FakeDatabase/FakeBooks.cs
@@ -11,7 +11,16 @@ public class FakeBooks
 
     public static List<Book> GenerateBooks(int count)
     {
-        var bookFaker = new Faker<Book>();
+        var authorFaker = new Faker<Author>()
+            .UseSeed(12345)
+            .RuleFor(b => b.Name, f => f.Name.FullName());
+
+        var bookFaker = new Faker<Book>()
+            .UseSeed(54321)
+            .RuleFor(b => b.Author, f => authorFaker.Generate())
+            .RuleFor(b => b.Title, f => f.Lorem.Sentence(4))
+            .RuleFor(b => b.Summary, f => f.Lorem.Sentences(5));
+
         var books = new List<Book>();
 
         for (var i = 0; i < count; i++)
